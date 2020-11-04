75c90238c46f3e84bdc550f36dffac65b1e14109
PWho <patrick@patrickwho.me>
2020-11-04 14:19:19 -0500

    Add some type-y goodness to exchange

--- lib/exchange.ts
+++ lib/exchange.ts
@@ -14,100 +14,105 @@ const fees = {
 // if there are any existing ASK orders on another exchange
 // that would allow for buying LOW in order to satisfy
 // the higher incoming bid.
-//
-// returns [opportunities {}, ok bool]
-function arbitrageBid(bid, exchange, existingAsks, compareExchange) {
-    const desiredLiquidity = parseFloat(bid.amount)
-    const opps = {}
+function arbitrageBid(
+    bid: OrderEventPayload,
+    exchange: string,
+    existingAsks: PriceToTotal,
+    compareExchange: string
+) : [OpportunitiesObj, boolean]
+{
+
+    const desiredLiquidity: number = parseFloat(bid.amount)
+    const opps: OpportunitiesObj = {}
 
     if (! desiredLiquidity) {
         return [opps, false]
     }
 
-    // identify arbitrage opportunities
-    {
+    // I need to sell at this price w/ this fee
+    const sellPrice: number = parseFloat(bid.price)
+    const exchangeFee: number = sellPrice * fees[exchange]
 
-        // I need to sell at this price w/ this fee
-        const sellPrice = parseFloat(bid.price)
-        const exchangeFee = sellPrice * fees[exchange]
+    // we have a "BID" => an offer to buy
+    // look at potential sellers to see if we can profit
+    for (const askPrice in existingAsks) {
 
-        // we have a "BID" => an offer to buy
-        // look at potential sellers to see if we can profit
-        for (const askPrice in existingAsks) {
+        const availableLiquidity: number = parseFloat(existingAsks.total)
 
-            const availableLiquidity = parseFloat(existingAsks.total)
-
-            if (! availableLiquidity) {
-                continue
-            }
-
-            let buyPrice
-            {
-                buyPrice = parseFloat(askPrice)
-                const buyFee = buyPrice * fees[compareExchange]
-                const profit = (sellPrice - buyPrice - (exchangeFee + buyFee))
-
-                if (profit <= 0) {
-                    console.log(`no profit buying from ${compareExchange} and selling to ${exchange}`, profit)
-                    continue
-                }
-            }
-
-            // we can buy from the other exchange and sell to this one
-            if (! opps[compareExchange][askPrice]) {
-                opps[compareExchange][askPrice] = {}
-            }
-
-            if (! opps[compareExchange][askPrice][bid.price]) {
-                opps[compareExchange][askPrice][bid.price] = {
-                    buy: 0,
-                    sell: 0,
-                }
-            }
-
-            opps[compareExchange][askPrice][bid.price].buy += availableLiquidity
-            opps[compareExchange][askPrice][bid.price].sell += desiredLiquidity
+        if (! availableLiquidity) {
+            continue
         }
 
-        return [opps, true]
+        let buyPrice: number
+        {
+            buyPrice = parseFloat(askPrice)
+            const buyFee: number = buyPrice * fees[compareExchange]
+            const profit: number = (sellPrice - buyPrice - (exchangeFee + buyFee))
+
+            if (profit <= 0) {
+                console.log(`no profit buying from ${compareExchange} and selling to ${exchange}`, profit)
+                continue
+            }
+        }
+
+        // we can buy from the other exchange and sell to this one
+        if (! opps[compareExchange][askPrice]) {
+            opps[compareExchange][askPrice] = {}
+        }
+
+        if (! opps[compareExchange][askPrice][bid.price]) {
+            opps[compareExchange][askPrice][bid.price] = {
+                buy: 0,
+                sell: 0,
+            }
+        }
+
+        opps[compareExchange][askPrice][bid.price].buy += availableLiquidity
+        opps[compareExchange][askPrice][bid.price].sell += desiredLiquidity
     }
+
+    return [opps, true]
 }
 
 // arbitrageAsk checks the incoming ASK order to see
 // if there are any existing BID orders on another exchange
 // that would allow for buying LOW on this incoming ASK
 // in order to sell HIGH to the existing BID order.
-//
-// returns [opportunities {}, ok bool]
-function arbitrageAsk(ask, exchange, existingBids, compareExchange) {
+function arbitrageAsk(
+    ask: OrderEventPayload,
+    exchange: string,
+    existingBids: PriceToTotal,
+    compareExchange: string
+) : [OpportunitiesObj, boolean]
+{
 
-    const availableLiquidity = parseFloat(ask.amount)
-    const opps = {}
+    const availableLiquidity: number = parseFloat(ask.amount)
+    const opps: OpportunitiesObj = {}
 
     if (! availableLiquidity) {
         return [opps, false]
     }
 
     // I can buy at this price w/ this fee:
-    const buyPrice = parseFloat(ask.price)
-    const buyFee = buyPrice * fees[exchange]
+    const buyPrice: number = parseFloat(ask.price)
+    const buyFee: number = buyPrice * fees[exchange]
 
     // we have an "ASK" => an offer to sell
     // look at potential buyers to see if we can profit
     for (const bidPrice in existingBids) {
 
-        const desiredLiquidity = parseFloat(existingBids[bidPrice].total)
+        const desiredLiquidity: number = parseFloat(existingBids[bidPrice].total)
 
         if (! desiredLiquidity) {
             continue
         }
 
-        let sellPrice
+        let sellPrice: number
         {
             // I can sell at this price w/ this fee:
             sellPrice = parseFloat(bidPrice)
-            const sellFee = sellPrice * fees[compareExchange]
-            const profit = (sellPrice - buyPrice - (buyFee + sellFee))
+            const sellFee: number = sellPrice * fees[compareExchange]
+            const profit: number = (sellPrice - buyPrice - (buyFee + sellFee))
 
             if (profit <= 0) {
                 console.log(`no profit buying from ${exchange} and selling to ${compareExchange}`, profit)
@@ -134,9 +139,10 @@ function arbitrageAsk(ask, exchange, existingBids, compareExchange) {
     return [opps, true]
 }
 
-function insertAskPrice(asks, price) {
-    const p = parseFloat(price)
-    const aa = [...asks]
+function insertAskPrice(asks: string[], price: string) : string[] {
+
+    const p: number = parseFloat(price)
+    const aa: string[] = [...asks]
 
     if (aa.length === 0) {
         aa.push((p).toFixed(8))
@@ -145,7 +151,7 @@ function insertAskPrice(asks, price) {
     }
 
     if (aa.length === 1) {
-        if (aa[0] > p) {
+        if (parseFloat(aa[0]) > p) {
             aa.unshift((p).toFixed(8))
         } else {
             aa.push((p).toFixed(8))
@@ -153,20 +159,20 @@ function insertAskPrice(asks, price) {
 
         return aa
     }
-    if (p < aa[0]) {
+    if (p < parseFloat(aa[0])) {
         aa.unshift((p).toFixed(8))
         return aa
     }
 
-    if (p > aa[aa.length-1]) {
+    if (p > parseFloat(aa[aa.length-1])) {
         aa.push((p).toFixed(8))
         return aa
     }
 
     // add the bid price to the ORDERED array of bids
     for (let i = 1; i < aa.length; i++) {
-        const lastPrice = aa[i-1]
-        const thisPrice = aa[i]
+        const lastPrice: number = parseFloat(aa[i-1])
+        const thisPrice: number = parseFloat(aa[i])
 
         if (p > lastPrice && p < thisPrice) {
             aa.splice(i, 0, (p).toFixed(8))
@@ -177,9 +183,10 @@ function insertAskPrice(asks, price) {
     return aa
 }
 
-function insertBidPrice(bids, price) {
-    const p = parseFloat(price)
-    const bb = [...bids]
+function insertBidPrice(bids: string[], price: string) : string[] {
+
+    const p: number = parseFloat(price)
+    const bb: string[] = [...bids]
 
     if (bb.length === 0) {
         bb.push((p).toFixed(8))
@@ -187,20 +194,20 @@ function insertBidPrice(bids, price) {
         return bb
     }
 
-    if (p > bb[0]) {
+    if (p > parseFloat(bb[0])) {
         bb.unshift((p).toFixed(8))
         return bb
     }
 
-    if (p < bb[bb.length-1]) {
+    if (p < parseFloat(bb[bb.length-1])) {
         bb.push((p).toFixed(8))
         return bb
     }
 
     // add the bid price to the ORDERED array of bids
     for (let i = 1; i < bb.length; i++) {
-        const lastPrice = bb[i-1]
-        const thisPrice = bb[i]
+        const lastPrice: number = parseFloat(bb[i-1])
+        const thisPrice: number = parseFloat(bb[i])
 
         if (p < lastPrice && p > thisPrice) {
             bb.splice(i, 0, (p).toFixed(8))
